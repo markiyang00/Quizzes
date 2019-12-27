2b457abcf68c838cda14d97c9db2feadfcc1d55a
morlokmag <morlokmag@gmail.com>
2019-12-27 14:48:29 +0200

    Work All Add and Alll Updated and All Del

--- Quizzes/Controllers/TestController.cs
+++ Quizzes/Controllers/TestController.cs
@@ -24,52 +24,16 @@ namespace Quizzes.Controllers
 			return View();
 		}
 
-		[HttpPost]
-		public IActionResult AddTest(TestUpdatedViewModel testModel)
-		{
-			if (ModelState.IsValid)
-			{
-				context.Tests.Add(testModel.Test);
-				context.SaveChanges();
-			}
-			return View(testModel);
-		}
-
-		public IActionResult TestPage(Test test)
-		{
-			var testPage = new TestPageViewModel() { Test = test };
-			var questions = context.Questions.AsNoTracking().Where(a => a.TestId == test.Id & !a.IsDel).ToList();
-			var i = 0;
-			var answers= new List<Answer>[questions.Count()];
-			foreach (var question in questions)
-			{
-				answers[i] = context.Answers.AsNoTracking().Where(a => a.QuestionId == question.Id).ToList();
-				i++;
-			}
-
-			testPage.Answers = answers;
-			testPage.Questions = questions;
-			return View(testPage);
-		}
-
 		public IActionResult AddQuestions(int testId)
 		{
 			var question=new Question(){TestId = testId};
-			return View(question);
+			var obg = new QuestionUpdatedViewModel
+			{
+				Question = question
+			};
+			return View(obg);
 		}
 
-		[HttpPost]
-		public IActionResult AddQuestions(Question question)
-		{
-			if (ModelState.IsValid)
-			{
-				context.Questions.Add(question);
-				context.SaveChanges();
-				var testBase = context.Tests.AsNoTracking().First(a => a.Id==question.TestId & !a.IsDel);
-				return RedirectToAction("TestPage", testBase);
-			}
-			return View(question);
-		}
 
 		public IActionResult AddAnswer(int id)
 		{
@@ -77,20 +41,7 @@ namespace Quizzes.Controllers
 			return View(answer);
 		}
 
-		[HttpPost]
-		public IActionResult AddAnswer(Answer answer)
-		{
-			if (ModelState.IsValid)
-			{
-				context.Answers.Add(answer);
-				context.SaveChanges();
-				var question = context.Questions.First(a => a.Id == answer.QuestionId & !a.IsDel);
-				var testBase = context.Tests.AsNoTracking().First(a => a.Id == question.TestId & !a.IsDel);
-				return RedirectToAction("TestPage", testBase);
-			}
-			return View(answer);
-		}
-
+		[Route("Test/UpdatedTest/{id}")]
 		public IActionResult UpdatedTest(int id)
 		{
 			var testBase = context.Tests.AsNoTracking().First(a => a.Id == id & !a.IsDel);
@@ -105,23 +56,15 @@ namespace Quizzes.Controllers
 		[HttpPost]
 		public IActionResult UpdatedTest(TestUpdatedViewModel testModel)
 		{
-			if (testModel.Test.Id == 0)
-			{
-				context.Tests.Add(testModel.Test);
-				context.SaveChanges();
-			}
-			else
-			{
-				context.Update(testModel.Test);
-				context.SaveChanges();
-			}
-
+			context.Update(testModel.Test);
+			context.SaveChanges();
 			testModel.Test = context.Tests.AsNoTracking().FirstOrDefault(a => a.Id == testModel.Test.Id & !a.IsDel);
 			testModel.Questions = context.Questions.AsNoTracking().Where(a => a.TestId == testModel.Test.Id & !a.IsDel)
 				.ToList();
 			return View(testModel);
 		}
 
+		[Route("Test/UpdatedQuestions/{id}")]
 		public IActionResult UpdatedQuestions(int id)
 		{
 			var question = context.Questions.First(a => a.Id == id & !a.IsDel);
@@ -136,16 +79,19 @@ namespace Quizzes.Controllers
 		[HttpPost]
 		public IActionResult UpdatedQuestions(QuestionUpdatedViewModel questionModel)
 		{
-			if (ModelState.IsValid)
+			if (questionModel.Question.Id == 0)
 			{
-				context.Update(questionModel.Question);
+				context.Questions.Add(questionModel.Question);
 				context.SaveChanges();
+				return RedirectToAction("UpdatedTest", new { id = questionModel.Question.TestId });
 			}
-
+			context.Update(questionModel.Question);
+			context.SaveChanges();
 			questionModel.Answers = context.Answers.AsNoTracking().Where(a => a.QuestionId == questionModel.Question.Id).ToList();
 			return View(questionModel);
 		}
 
+		[Route("Test/UpdatedAnswer/{id}")]
 		public IActionResult UpdatedAnswer(int id)
 		{
 			var answer = context.Answers.AsNoTracking().First(a => a.Id == id);
@@ -157,6 +103,12 @@ namespace Quizzes.Controllers
 		{
 			if (ModelState.IsValid)
 			{
+				if (answer.Id == 0)
+				{
+					context.Answers.Add(answer);
+					context.SaveChanges();
+					return RedirectToAction("UpdatedTest", new { id = answer.QuestionId });
+				}
 				context.Update(answer);
 				context.SaveChanges();
 			}
@@ -169,7 +121,7 @@ namespace Quizzes.Controllers
 			elem.IsDel = true;
 			context.Update(elem);
 			context.SaveChanges();
-			return RedirectToAction("Admin","Admin",elem);
+			return RedirectToAction("Admin","Admin");
 		}
 
 		public IActionResult DelQuestions(int id)
@@ -178,8 +130,7 @@ namespace Quizzes.Controllers
 			elem.IsDel = true;
 			context.Update(elem);
 			context.SaveChanges();
-			var testBase = context.Tests.AsNoTracking().First(a => a.Id == elem.TestId & !a.IsDel);
-			return RedirectToAction("TestPage", testBase);
+			return RedirectToAction("UpdatedTest", new { id = elem.TestId});
 		}
 
 		public IActionResult DelAnswer(int id)
@@ -187,10 +138,7 @@ namespace Quizzes.Controllers
 			var elem = context.Answers.AsNoTracking().First(p => p.Id == id);
 			context.Remove(elem);
 			context.SaveChanges();
-			var question = context.Questions.First(a => a.Id == elem.QuestionId & !a.IsDel);
-			var testBase = context.Tests.AsNoTracking().First(a => a.Id == question.TestId & !a.IsDel);
-			return RedirectToAction("TestPage", testBase);
+			return RedirectToAction("UpdatedQuestions",new{id=elem.QuestionId});
 		}
-
 	}
 }
